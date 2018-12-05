1e2445aacb03e9b3fb49d0e30b7dc946c56eee0e
Collin Argo <collinargo@gmail.com>
2018-12-05 12:35:57 -0500

    update task name h1 with search box

--- src/TaskList.js
+++ src/TaskList.js
@@ -7,7 +7,8 @@ export default class TaskList extends Component {
         super(props)
         this.state = {
             searchTerm: '',
-            tasks: []
+            tasks: [],
+            selectedTask: null
         }
     }
 
@@ -42,6 +43,28 @@ export default class TaskList extends Component {
         })
     }
 
+    _selectTask = taskToSelect => {
+        // update search box text to task name
+        this.setState({
+            searchTerm: taskToSelect.name,
+            selectedTask: taskToSelect
+        })
+    }
+
+    _completeTask = taskToComplete => {
+        fetch('/test-react-complete', {
+            method: 'update',
+            body: JSON.stringify(taskToComplete),
+            headers: {'Content-Type': 'application/json'}
+        })
+        .then(res => res.json())
+        .then(tasks => {
+            this.setState({
+                tasks
+            })
+        })
+    }
+
     _deleteTask = iDToDelete => {
         fetch('/test-react-delete', {
             method: 'delete', 
@@ -68,7 +91,11 @@ export default class TaskList extends Component {
                 onChange={event => this._updateSearch(event.target.value)}
                 />
                 <Tasks tasks={this.state.tasks} 
+                selectTask={this._selectTask}
+                completeTask={this._completeTask}
                 deleteTask={this._deleteTask}
+                searchTerm={this.state.searchTerm}
+                selectedTask={this.state.selectedTask}
                 />
             </div>
         )
--- src/Tasks.js
+++ src/Tasks.js
@@ -4,7 +4,18 @@ import Task from './Task'
 export default function Tasks(props){
     return (
         <div className="Tasks">
-            {props.tasks.map((task, index) => <Task task={task} key={index} deleteTask={props.deleteTask} index={index}/>)}
+            {props.tasks.map(task => {
+            return (
+            <Task
+            task={task} 
+            key={task.id} 
+            selectTask={props.selectTask}
+            completeTask={props.completeTask}
+            deleteTask={props.deleteTask} 
+            searchTerm={props.searchTerm}
+            selectedTask={props.selectedTask}
+            />
+            )})}
         </div>
     )
 }
