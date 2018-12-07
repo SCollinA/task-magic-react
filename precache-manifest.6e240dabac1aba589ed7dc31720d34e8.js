db4e12b8c8e14928fd35aec66c3f4e994b42fd86
Collin Argo <collinargo@gmail.com>
2018-12-07 14:31:28 -0500

    Updates

---
+++ precache-manifest.6e240dabac1aba589ed7dc31720d34e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc1cf176bf7dd30ad219",
+    "url": "/taskmagic/static/js/runtime~main.cc1cf176.js"
+  },
+  {
+    "revision": "3a5b13351b251effdec8",
+    "url": "/taskmagic/static/js/main.3a5b1335.chunk.js"
+  },
+  {
+    "revision": "2d4960d1b3635cbe9d57",
+    "url": "/taskmagic/static/js/1.2d4960d1.chunk.js"
+  },
+  {
+    "revision": "3a5b13351b251effdec8",
+    "url": "/taskmagic/static/css/main.0ca54abc.chunk.css"
+  },
+  {
+    "revision": "553382d8edbd57065d4bcdbc2d56241d",
+    "url": "/taskmagic/index.html"
+  }
+];
