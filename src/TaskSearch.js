bd61c3f77b80b27fc7763b1f419c104de7b3b5c8
Collin Argo <collinargo@gmail.com>
2018-12-11 17:25:32 -0500

    fix task input again

--- src/TaskSearch.js
+++ src/TaskSearch.js
@@ -2,20 +2,19 @@ import React from 'react'
 
 export default function TaskSearch(props) {
     return (
-        <form className={`TaskSearch`} 
+        <form className={`TaskSearch stickyContain`} 
         style={props.style}
         onSubmit={props.onSubmit} 
         onReset={props.onReset}>
-            <div className="taskInput hackerStyle">
-                <label name="inputTask">
-                    Input Task:<input
-                    value={props.searchTerm} 
-                    onChange={props.onChange} 
-                    type='text'
-                    name='inputTask'
-                    />
-                </label>
-            </div>
+            <label className="taskInput hackerStyle"
+            name="inputTask">
+                Input Task:<input
+                value={props.searchTerm} 
+                onChange={props.onChange} 
+                type='text'
+                name='inputTask'
+                />
+            </label>
             <button className="hackerStyle" type="reset">Cancel</button>
         </form>
     )
