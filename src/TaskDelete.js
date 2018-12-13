6861a26464e2bf66237444630a507ab0998b6dcc
Collin Argo <collinargo@gmail.com>
2018-12-13 17:15:52 -0500

    add TaskDelete file

---
+++ src/TaskDelete.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TaskDelete(props) {
+    return (
+        <div className="TaskDelete">
+            <h1>select task</h1>
+        </div>
+    )
+}
